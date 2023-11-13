27c57ac22975e51cf93059ce9455e0d18e6d67d3
Difa <difafisabill@gmail.com>
2023-11-13 22:01:26 +0700

    Navigation

---
+++ src/assets/Images/index.js
@@ -0,0 +1,25 @@
+import sprite from './sprite.jpg';
+import box from './box.png';
+import bill from './bill.png';
+import more from './application.png';
+import scooter from './scooter.png';
+import shopping from './shopping.png';
+import spoon from './takeaway.png';
+import train from './train.png';
+import car from './car.png';
+import driver from './food.jpg';
+import download from './download.png';
+
+export {
+  sprite,
+  box,
+  bill,
+  more,
+  scooter,
+  shopping,
+  spoon,
+  train,
+  car,
+  driver,
+  download,
+};
